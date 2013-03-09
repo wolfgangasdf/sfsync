05547b208d9af583b72bdb08005d14e5033b89f2
wolfgang <wolfgang@localhost>
2013-03-09 12:19:51 +0100

    startup script version update

--- sfsync-run.sh
+++ sfsync-run.sh
@@ -2,4 +2,4 @@
 # fix for mac java wrong filename encoding
 export LC_CTYPE="UTF-8"
 
-java -Xmx128m -jar target/scala-2.10/sfsync_2.10.0-0.1/sfsync_2.10.0-0.1.jar
+java -Xmx128m -jar target/scala-2.10/sfsync_2.10.0-0.2/sfsync_2.10.0-0.2.jar
